584973cebe9ea518caa89072cb2864b43180c904
Cesclade <vadim.kupik@gmail.com>
2024-06-17 22:15:29 +0300

    фильтры

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
-function setActiveFilter(e){
-    const elements = document.getElementsByClassName("active-f");
-    Array.from(elements).forEach((el)=>{
-        el.classList.remove("active-f")
+// function setActiveFilter(e){
+//     const elements = document.getElementsByClassName("active-f");
+//     Array.from(elements).forEach((el)=>{
+//         el.classList.remove("active-f");
 
-    })
-    e.classList.add("active-f")
+//     })
+//     e.classList.add("active-f");
 
-}
+// }
+
 const taskInput = document.getElementById("task");
 const addTaskButton = document.getElementById("addTask");
 const taskList = document.getElementById("taskList");
+const filter = document.querySelector('.filter')
 taskList.innerHTML = localStorage.getItem('todo');
 
+
+
 addTaskButton.addEventListener("click", () => {
-	const task = taskInput.value;
+    const task = taskInput.value;
     if (task.trim() === "") {
-		alert("Don't add task if task is empty")
+        alert("Don't add task if task is empty")
 		return;
 	}
 	const taskItem = document.createElement("div");
@@ -26,11 +30,47 @@ addTaskButton.addEventListener("click", () => {
     <button class="markImportant">MARK IMPORTANT</button>
     <button class="delete"></button>
     `;
-
+    
     taskList.append(taskItem);
 	taskInput.value = "";
     updateList();
+    
+});
 
+filter.addEventListener("click", (e) => {
+    const elements = document.querySelectorAll("button");
+    if (!e.target.matches('button')) return;
+    Array.from(elements).forEach((el)=>{
+        el.classList.remove("active-f");
+
+    })
+
+    e.target.classList.add("active-f");
+
+    tasks = taskList.querySelectorAll(".task");
+    tasks.forEach((task)=>{
+        if (e.target.classList.contains("active-b")){
+            if (task.classList.contains("done")){
+                task.classList.add("hiden");
+            }
+            else {
+                task.classList.remove("hiden");
+            }
+        }
+        else if (e.target.classList.contains("done-b")){
+            if (task.classList.contains("active")){
+                task.classList.add("hiden");
+            }
+            else {
+                task.classList.remove("hiden");
+            }
+        }
+        else {
+            task.classList.remove("hiden");
+        }
+        
+    });
+    
 });
 
 function updateList(){
@@ -44,6 +84,7 @@ function updateList(){
 
 taskList.addEventListener("click", (event) => {
     const taskItem = event.target.parentElement;
+
     if (event.target.classList.contains("markDone")) {
         if (taskItem.classList.contains('done')){
             taskItem.classList.remove('done');
@@ -68,3 +109,5 @@ taskList.addEventListener("click", (event) => {
     }
     updateList();
 });
+
+ 
